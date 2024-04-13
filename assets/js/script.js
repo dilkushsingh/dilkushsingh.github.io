25bd9fc6adab14506ff8e7aa7f25a5b5943d1ee0
Dilkush Singh <work.dilkushsingh@gmail.com>
2024-04-13 15:14:12 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+
+//Smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+//Navbar toggle
+// $('ul.nav').addClass('show');
+
+//Add animations to menu
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+//Download CV
+document.getElementById('downloadButton').addEventListener('click', function() {
+    var link = document.createElement('a');
+    link.href = 'assets/Dilkush_Singh_Resume.pdf';
+    link.download = 'Dilkush Singh Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
